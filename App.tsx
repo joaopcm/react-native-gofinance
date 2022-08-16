aa0271afc90996e98c577766f015bd379a502265
João Melo <jopcmelo@gmail.com>
2022-08-16 15:55:33 -0300

    feat: add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 import { ThemeProvider } from 'styled-components';
+
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins'
 
 import theme from './src/global/styles/theme'
 import { Dashboard } from './src/pages/Dashboard';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Poppins_400Regular,
+          Poppins_500Medium,
+          Poppins_700Bold,
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <ThemeProvider theme={theme}>
-      <Dashboard />
-    </ThemeProvider>
+    <View
+      onLayout={onLayoutRootView}
+      style={{
+        flex: 1,
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <Dashboard />
+      </ThemeProvider>
+    </View>
   );
 }
